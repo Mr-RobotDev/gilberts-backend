9ad08e0c72828191360c4b36be9e9f6c456fca55
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-03-07 01:29:07 +0500

    update: update unit mode dto values constraint

--- src/mode-settings/dto/update-unit-mode.dto.ts
+++ src/mode-settings/dto/update-unit-mode.dto.ts
@@ -3,6 +3,6 @@ import { IsIn, IsInt, IsNotEmpty } from 'class-validator';
 export class UpdateUnitModeDto {
   @IsInt()
   @IsNotEmpty()
-  @IsIn([0, 1, 2, 3, 4])
+  @IsIn([0, 1])
   value: number;
 }
